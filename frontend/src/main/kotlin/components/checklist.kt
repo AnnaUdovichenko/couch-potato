f4e71d3a54c78d8b7b290bb9bf9122bae3861020
Anna Udovichenko <anuta.udovichenko@gmail.com>
2018-09-02 21:58:50 +0300

    first steps with react

---
+++ frontend/src/main/kotlin/components/checklist.kt
@@ -0,0 +1,41 @@
+package components
+
+import react.*
+import react.dom.*
+
+interface OptionsProps: RProps {
+    var options: Array<String>
+}
+
+interface InterestProps : RProps{
+    var text: String
+}
+
+interface  InterestState: RState{
+    var checked: Boolean
+}
+
+class Interest: RComponent<InterestProps, InterestState>(){
+    override fun RBuilder.render(){
+        p{+props.text }
+    }
+}
+
+fun RBuilder.interest(text: String) = child(Interest::class){
+    attrs.text = text
+}
+
+class CheckList: RComponent<OptionsProps, RState>(){
+    override fun RBuilder.render(){
+        div{
+                props.options.map{
+                    interest(it)
+                }
+            }
+
+    }
+}
+
+fun RBuilder.checklist(options: Array<String>) = child(CheckList::class){
+    attrs.options = options
+}
