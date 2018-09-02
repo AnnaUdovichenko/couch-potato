4f6c6895bb7302223a49059c448eb867e1c981f8
Anna Udovichenko <anuta.udovichenko@gmail.com>
2018-09-02 17:19:01 +0300

    frontend helloworld

---
+++ frontend/src/main/Kotlin/main.kt
@@ -0,0 +1,32 @@
+
+// import kotlinx.html.*
+// import kotlin.browser.*
+import react.*
+import react.dom.*
+import kotlin.browser.document
+import kotlin.browser.window
+
+interface WelcomeProps: RProps {
+    var name: String
+}
+ 
+class Welcome: RComponent<WelcomeProps, RState>() {
+     override fun RBuilder.render() {
+        div {
+            +"Hello, ${props.name}"
+        }
+    }
+}
+ 
+fun RBuilder.welcome(name: String = "John") = child(Welcome::class) {
+    attrs.name = name
+}
+
+fun main(args: Array<String>) {
+    window.onload = {
+        val root = document.getElementById("root") ?: throw IllegalStateException()
+        render(root) {
+            welcome("Anuta")
+        }
+    }
+}
