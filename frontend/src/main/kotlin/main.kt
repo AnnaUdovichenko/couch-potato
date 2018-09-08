a907f660ece57f53be48d997f0085695d5cbed5f
Anna Udovichenko <anuta.udovichenko@gmail.com>
2018-09-08 22:32:51 +0300

    Add Select interest component and submit button

--- frontend/src/main/kotlin/main.kt
+++ frontend/src/main/kotlin/main.kt
@@ -5,29 +5,12 @@ import kotlin.browser.document
 import kotlin.browser.window
 import components.*
 
-interface WelcomeProps: RProps {
-    var name: String
-}
- 
-class Welcome: RComponent<WelcomeProps, RState>() {
-     override fun RBuilder.render() {
-        div {
-            +"Hello, ${props.name}"
-        }
-    }
-}
-
- 
-fun RBuilder.welcome(name: String = "John") = child(Welcome::class) {
-    attrs.name = name
-}
 
 fun main(args: Array<String>) {
     window.onload = {
         val root = document.getElementById("root") ?: throw IllegalStateException()
         render(root) {
-            welcome("Anuta")
-            checklist(arrayOf("a", "b", "c"))
+            checklist(arrayOf("abcd", "bcde", "cdef"))
         }
     }
 }
