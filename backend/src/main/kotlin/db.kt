9eb49d7fecaaa4370cd279b3ce3bfac27b586f78
Anna Udovichenko <anuta.udovichenko@gmail.com>
2018-09-23 21:56:51 +0300

    Switch db to mlab

--- backend/src/main/kotlin/db.kt
+++ backend/src/main/kotlin/db.kt
@@ -2,7 +2,8 @@ package db
 
 
 val mongoClient = main.require("mongodb").MongoClient
-val url = "mongodb://localhost:27017/data"
+val url = main.require("process").env.MONGOLAB_URI;
+//val url = "mongodb://potato:lazy-c0uch-pot%40to@ds213053.mlab.com:13053/couch-potato"
 
 
 data class Idea(val text: String, val interests: Array<String>)
@@ -17,12 +18,13 @@ data class InterestList(val interests: Array<String>)
 
 
 fun loadInterests(callback: (HashSet<String>?) -> Unit){
+    console.log("Url: $url")
     mongoClient.connect(url) { err, db ->
         if (err) {
             console.log("Connection error: $err")
         } else {
             console.log("Connected!")
-            val dbo = db.db("data")
+            val dbo = db.db("couch-potato")
             dbo.collection("ideas").find(object{}, projection).toArray { err, res ->
                 if (err) {
                     console.log("error: $err")
