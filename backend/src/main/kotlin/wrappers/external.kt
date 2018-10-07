7d032b071392d8788cb9290b13b4fac1ca1a5caa
Anna Udovichenko <anuta.udovichenko@gmail.com>
2018-10-07 23:18:07 +0300

    add forgotten file

---
+++ backend/src/main/kotlin/wrappers/external.kt
@@ -0,0 +1,9 @@
+package wrappers
+
+external fun require(module:String):dynamic
+
+val path = require("path")
+external val __dirname: String
+val bodyParser = require("body-parser")
+
+val express = require("express")
